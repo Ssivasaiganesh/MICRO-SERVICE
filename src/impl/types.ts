f6edf1243338832ee22199bf3db17167d055ae9c
Ssivasaiganesh <sudarsanasivasaiganesh@gmail.com>
2022-09-21 18:19:43 +0530

    first commit

---
+++ src/impl/types.ts
@@ -0,0 +1,13 @@
+import { UserApiImpl } from "./user/types";
+import { ApiImplementation } from "../../dict/types";
+
+
+export class serviceApiimpl implements ApiImplementation {
+    user:UserApiImpl=new UserApiImpl
+}
+
+
+
+
+
+
